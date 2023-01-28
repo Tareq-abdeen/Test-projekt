ed61292a28f0d022542ce080923153a45f4ce692
Tareq-abdeen <abdeentareq@gmail.com>
2023-01-28 19:56:35 +0100

    for, while, dowhile, if,

---
+++ java/java_index.js
@@ -0,0 +1,103 @@
+/*age = prompt('enter your age')
+age < 3 && age >= 1?
+    document.write('you are child')
+    : age == 3 ?
+        document.write('super AGE')
+        : age > 3 ?
+        document.write('you are old')
+        : document.write('you are not reborn');
+  */
+
+/*   age = prompt('enter your age')
+   age>3?
+   document.write('you are child')
+   : document.write('you are not mit old')
+   age ==3?
+   document.write('you are gros')
+   :document.write('you are süß');
+
+
+role=prompt('enter your role');
+switch(role){
+case 'admin':
+document.write('you are admin');
+break;
+case 'moderator'
+:document.write('you are moderator');
+break;
+case 'user':
+document.write('you are user');
+break;
+}*/
+
+/*for(var i=0; i<=10; i++)
+{
+  document.write('hello world'+ i +'<br>'+'<br>'); 
+}
+*/
+
+
+/*
+var x='ahmad alschamei '; 
+for (i=x.length-1; i>=0; i--){
+  document.write(x[i] + '<br>');
+}*/
+
+
+
+/*
+let cars = ['BMW', 'Mercedes', 'Honda'];
+let models = [2020, 2022, 2023];
+let colors = ['red', 'black', 'yellow'];
+
+for (let i = 0; i < cars.length; i++) {
+  document.write('car>>' + cars[i] + '<br>');
+  for (let j = 0; j < models.length; j++) {
+
+    document.write('model :' + models[j] + '<br>');
+  }
+  for (let x = 0; x < models.length; x++) {
+
+    document.write('model :' + colors[x] + '<br>');
+  }
+
+  document.write('__________' + '<br>');
+}
+*/
+
+
+/*
+ for with if
+let users=['ali', 'amza', 'mazen', 42,789,7886,'ali1','amza1', 'mazen1']; 
+document.write('users: ' + '<br>');
+for(let i=0; i<users.length;i++){
+
+  if( users[i]=='ali1' || typeof users[i]=='number' ){
+    continue;
+  }
+  document.write( users[i]+ '<br>');
+}
+*/
+
+/*
+
+let i=0;
+while(i<3){
+  document.write('<h1> hello world </h1>' +'<br>');
+  i++;
+}
+*/
+/*
+let i=0;
+while(false){
+  document.write('<h1> hello world </h1>' +'<br>');
+  i++;
+}*/
+/*
+let i=0;
+do{
+  document.write('<h1> hello world </h1>' +'<br>');
+  i++;
+}
+while(i<3);
+document.write(i);*/
